d754ba9ca7033ac8c3924652a6d6f49aaed19d9e
Gustavo Javier Barea Paci <gustavobarea@gmail.com>
2026-05-17 20:42:07 -0300

    Remove internal files from public repository

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Instalando dependencias...
-pip install streamlit pandas numpy plotly openpyxl --quiet
-echo Iniciando Solar Decomp...
-streamlit run app_solar.py
-pause
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Instalando dependencias..."
-pip install streamlit pandas numpy plotly openpyxl --quiet
-echo "Iniciando Solar Decomp..."
-streamlit run app_solar.py
